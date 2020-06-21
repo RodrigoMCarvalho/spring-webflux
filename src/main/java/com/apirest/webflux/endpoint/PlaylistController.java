c033e735af4b5b944055b9ecf59baf3822311ed1
RodrigoMCarvalho <rodrigo_moreira84@hotmail.com>
2020-06-21 20:43:20 -0300

    criado document, service, repository e controller

---
+++ src/main/java/com/apirest/webflux/endpoint/PlaylistController.java
@@ -0,0 +1,41 @@
+package com.apirest.webflux.endpoint;
+
+import com.apirest.webflux.document.Playlist;
+import com.apirest.webflux.service.PlaylistService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+@RestController
+@RequestMapping("/playlist")
+public class PlaylistController {
+
+    @Autowired
+    private PlaylistService playlistService;
+
+    @GetMapping
+    public Flux<Playlist> getPlaylist() {
+        return playlistService.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public Mono<Playlist> getPlaylistById(@PathVariable("id") String id) {
+        return playlistService.findById(id);
+    }
+
+    @PostMapping
+    public Mono<Playlist> save(@RequestBody Playlist playlist) {
+        return playlistService.save(playlist);
+    }
+
+    @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.ACCEPTED)
+    public Mono<Void> delete(@PathVariable("id") String id) {
+        return playlistService.delete(id);
+    }
+
+
+}
